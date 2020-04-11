291bee37458f9fa52311211bd374532f05cc9d58
pberrio <berriop@gmail.com>
2020-04-11 17:11:06 -0500

    avoding the empty spaces of the a element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-
 function Card(props) {
     return (  
         <div className="card d-flex-md-row project-container mx-3 px-3 pt-3 mb-3 pb-1 rounded">
-            <a href='/' name="targetprojects" style={{visibility: 'hidden'}}>Go to projects</a>
+            <a href='/' name="targetprojects" style={{visibility: 'hidden', position: 'absolute'}}>Go to projects</a>
             <img className="card-img-top project-pic border-left border-right border-bottom"
                 src={`https://firebasestorage.googleapis.com/v0/b/packyfood-80156.appspot.com/o/${props.imgLogo}`}
                 alt={`${props.title}-cardImage`} />
@@ -13,7 +12,7 @@ function Card(props) {
                 <p className="card-text">{props.text}</p>
                 <a href={props.botonVerMas} target="blank_" className="btn btn-sm btn-primary">See more</a>
             </div>
-        </div> 
+        </div>
     )
 }
 
