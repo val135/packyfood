9e5e2bd4dc21258e7d9d8011d044628e2ce4e90d
Valentina <valen_135@hotmail.com>
2020-04-16 21:06:41 -0500

    errores de responsive

--- src/components/DescriptionPacky.js
+++ src/components/DescriptionPacky.js
@@ -11,16 +11,16 @@ const DescriptionPacky = () => {
             {/* primer row con una columna */}
             <div className="row text-center cont-description-packy">
                 <div className="col-xs-12 col-sm-12 col-md-12">
-                    <p>A través de PackyFood puedes obtener un paquete de comida sorpresa o “Packy” de los mejores restaurantes <br />de la ciudad a precios nunca antes vistos.
-                    Nuestros aliados se encargan de ofrecer productos elaborados durante el día que por diferentes motivos no fueron vendidos, pero que cumplen 100% con todas las condiciones de salubridad para ser entregados a personas como tú, que  seguro los disfrutarán.<br />
+                    <p>A través de PackyFood puedes obtener un paquete <b>sorpresa o “Packy”</b> de restaurantes y locales de comida <br />a precios realmente económicos.
+                    Nuestros aliados se encargan de ofrecer productos elaborados durante el día <br />que por diferentes motivos no fueron vendidos, pero que cumplen 100% con todas las condiciones de salubridad para ser entregados <br />a personas como tú, que  seguro los disfrutarán.<br />
                         <br /><b>
-                            Di no al desperdicio de alimentos y únete a nosotros para luchar por un mundo más sostenible.<br />
+                            Di no al desperdicio de alimentos y únete a nosotros para buscar un mundo más sostenible.<br />
                     Sigue los pasos a continuación para obtener hoy mismo tu “Packy”.</b></p>
                 </div>
             </div>
 
             {/* segundo row con tres columnas */}
-            <div className="row text-center w-75 cont-steps justify-content-around">
+            <div className="row text-center cont-steps justify-content-around">
                 <div className="col-xs-12 col-sm-12 col-md-3 steps">
                     <img className="step justify-content-center" src="./packy-step1.png" alt="PackyFood" />
                 </div>
