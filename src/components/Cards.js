c7a68ffa0445d5b25529bda4a80452f24ead62fe
Valentina <valen_135@hotmail.com>
2020-04-16 21:47:03 -0500

    ancla restaurantes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ const Cards = (props) => {
     const restaurantsToShow = (props.filter === '') ? restaurantsData : restaurantsData.filter(restaurant => restaurant.category === props.filter);
     return (
         <div className="container-fluid">
-            <a href='/' name="target restaurantes" alt='restaurant' style={{visibility: 'hidden', position: 'absolute'}}>Go to restaurant</a>
+            <a href='/' name="targetrestaurantes" alt='restaurant' style={{visibility: 'hidden', position: 'absolute'}}>Go to restaurant</a>
             <h3 className="text-center selection-restaurantes">Selecciona tu restaurante favorito y compra un paquete sorpresa por un precio muy económico</h3>
             <div className="row container-fluid cont-restaurantes mx-auto justify-content-center w-100"> 
                 {restaurantsToShow.map((restaurantData, index) => <Card key={index} {...restaurantData} />)}
